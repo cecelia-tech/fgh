fe99d2e81db717b543a6158d9ca38807d1bf1bd3
vita <vitagriciute@live.com>
2021-12-15 19:09:50 +0200

    Small Changes

--- Data/Kazkas1/Program.cs
+++ Data/Kazkas1/Program.cs
@@ -6,7 +6,7 @@ namespace Kazkas1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!gjdgd");
         }
     }
 }
