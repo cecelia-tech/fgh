36bc3d3024ec7b04a0c3bb114d6d819edbbac6fb
vita <vitagriciute@live.com>
2021-12-15 19:08:39 +0200

    Initial check-in of module Flower

---
+++ Data/Kazkas1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kazkas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
