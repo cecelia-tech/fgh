24d5f671e890acb1d3c01d1894c8699f7053f92b
vita <vitagriciute@live.com>
2021-12-15 20:55:22 +0200

    Lecture created

---
+++ Data/Kazkas1/Lecture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TrainingManagementSystem.Trainings;
+
+namespace TrainingManagementSystem.Lectures
+{
+    public class Lecture : Training
+    {
+        public string Topic { get; set; } = string.Empty;
+
+        public Lecture(string description, string topic) : base(description)
+        {
+            Topic = topic;
+        }
+    }
+}
